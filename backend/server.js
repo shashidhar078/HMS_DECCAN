fe5c485b811fd51391d39bff8a99ab514dda9164
shashidhar078 <nagunurishashidhar@gmail.com>
2025-06-14 16:48:56 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -84,7 +84,7 @@ const { errorHandler } = require('./middlewares/errorMiddleware');
 app.use(errorHandler);
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
 // Handle unhandled promise rejections
